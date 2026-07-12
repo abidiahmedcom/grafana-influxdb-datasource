086efeafe67eef79e155ec286199041af8aeb118
Adam Yeats <adam.yeats@grafana.com>
2026-07-12 03:16:29 +0100

    feat(annotations): support annotation queries in SQL mode

--- src/datasource_sql.test.ts
+++ src/datasource_sql.test.ts
@@ -2,6 +2,7 @@ import { lastValueFrom } from 'rxjs';
 
 import { type SQLQuery } from '@grafana/sql';
 
+import { FSQLEditor } from './components/editor/query/fsql/FSQLEditor';
 import InfluxDatasource from './datasource';
 import { getMockDSInstanceSettings, mockBackendService, mockTemplateSrv } from './mocks/datasource';
 import { mockInfluxQueryRequest } from './mocks/request';
@@ -32,6 +33,28 @@ describe('InfluxDB SQL Support', () => {
       expect(replaceMock.mock.calls[1][0]).toBe(
         `SELECT "$interpolationVar2", time FROM iox.$interpolationVar WHERE time >= $__timeFrom AND time <= $__timeTo`
       );
+    });
+  });
+
+  describe('annotations', () => {
+    const ds = new InfluxDatasource(getMockDSInstanceSettings({ version: InfluxVersion.SQL }), templateSrv);
+
+    it('should use the SQL query editor with standard annotation support', () => {
+      expect(ds.annotations?.QueryEditor).toBe(FSQLEditor);
+      expect(ds.annotations?.prepareAnnotation).toBeUndefined();
+    });
+  });
+
+  describe('filterQuery', () => {
+    const ds = new InfluxDatasource(getMockDSInstanceSettings({ version: InfluxVersion.SQL }), templateSrv);
+
+    it('should run queries with rawSql', () => {
+      expect(ds.filterQuery(sqlQuery)).toBe(true);
+    });
+
+    it('should skip queries without rawSql', () => {
+      expect(ds.filterQuery({ refId: 'x' })).toBe(false);
+      expect(ds.filterQuery({ refId: 'x', rawSql: '' })).toBe(false);
     });
   });
 });
